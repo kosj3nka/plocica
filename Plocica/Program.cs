8003e0c17fd5f92a022d13419caa9365e1d08860
Gita <projicgita@gmail.com>
2026-08-23 09:12:22 +0200

    Use Managed Identity for Blob Storage in production

--- Plocica/Program.cs
+++ Plocica/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
@@ -8,28 +9,49 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages(options =>
-{
-    options.Conventions.AuthorizeFolder("/Admin");
-    options.Conventions.AllowAnonymousToPage("/Admin/Login");
-});
+                               {
+                                   options.Conventions.AuthorizeFolder("/Admin");
+                                   options.Conventions.AllowAnonymousToPage("/Admin/Login");
+                               });
 
 builder.Services.AddDbContext<AppDbContext>(o =>
-    o.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+                                            o.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.AddSingleton(_ => new BlobServiceClient(builder.Configuration["Blob:ConnectionString"]));
+builder.Services.AddSingleton(_ =>
+                              {
+                                  var config = builder.Configuration;
+                                  var connectionString = config["Blob:ConnectionString"];
+
+                                  if (!string.IsNullOrWhiteSpace(connectionString))
+                                  {
+                                      // Lokalni razvoj (npr. Azurite) koristi connection string.
+                                      return new BlobServiceClient(connectionString);
+                                  }
+
+                                  var accountUrl = config["Blob:AccountUrl"]
+                                      ?? throw new InvalidOperationException("Postavi Blob:ConnectionString (lokalno) ili Blob:AccountUrl (produkcija, Managed Identity).");
+
+                                  var managedIdentityClientId = config["Blob:ManagedIdentityClientId"];
+                                  var credential = string.IsNullOrWhiteSpace(managedIdentityClientId)
+                                      ? new DefaultAzureCredential()
+                                      : new ManagedIdentityCredential(managedIdentityClientId);
+
+                                  // Produkcija: bez tajni, autentikacija preko Managed Identity Web App-a.
+                                  return new BlobServiceClient(new Uri(accountUrl), credential);
+                              });
 builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
 builder.Services.AddSingleton<LoginThrottleService>();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
-    {
-        options.LoginPath = "/Admin/Login";
-        options.LogoutPath = "/Admin/Logout";
-        options.AccessDeniedPath = "/Admin/Login";
-        options.ExpireTimeSpan = TimeSpan.FromDays(7);
-        options.SlidingExpiration = true;
-        options.Cookie.HttpOnly = true;
-    });
+               {
+                   options.LoginPath = "/Admin/Login";
+                   options.LogoutPath = "/Admin/Logout";
+                   options.AccessDeniedPath = "/Admin/Login";
+                   options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                   options.SlidingExpiration = true;
+                   options.Cookie.HttpOnly = true;
+               });
 
 var app = builder.Build();
 
